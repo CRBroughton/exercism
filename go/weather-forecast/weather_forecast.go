967eca875c1f856e30f469c303b84dd7b096a423
CRBroughton <crbroughton@posteo.uk>
2023-03-17 21:43:07 +0000

    feat: :white_check_mark: complete 'Weather Forcast' go exercise

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,16 @@
+// Package weather - Forecasts the current weather conditions for a given city.
+// Provide the Forecast function with a city name and condition
+// to recieve the forcast.
 package weather
 
+// CurrentCondition - Sets the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation - Set the current weather location.
 var CurrentLocation string
 
+// Forecast - Returns the Forecast.
+// Requires a city and condition arguments.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
