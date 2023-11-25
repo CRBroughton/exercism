fbaafc67777179837f816173a1b734901d56954a
CRBroughton <crbroughton@posteo.uk>
2023-11-25 17:35:05 +0000

    feat(Elixir): :white_check_mark: complete 'Lasagma' elixir exercise

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,19 @@
+defmodule Lasagna do
+  # Please define the 'expected_minutes_in_oven/0' function
+  @spec expected_minutes_in_oven() :: number()
+  def expected_minutes_in_oven, do: 40
+
+  # Please define the 'remaining_minutes_in_oven/1' function
+  @spec remaining_minutes_in_oven(number()) :: number()
+  def remaining_minutes_in_oven(minutes), do: expected_minutes_in_oven() - minutes
+
+  # Please define the 'preparation_time_in_minutes/1' function
+  @spec preparation_time_in_minutes(number()) :: number()
+  def preparation_time_in_minutes(layers), do: layers * 2
+
+  # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, minutes), do: preparation_time_in_minutes(layers) + minutes
+
+  # Please define the 'alarm/0' function
+  def alarm, do: "Ding!"
+end
